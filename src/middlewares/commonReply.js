577d640fa36c01ccfc3b33247312075febeb8e39
h41ku <h41ku.c0de@ya.ru>
2025-02-27 14:50:56 +0300

    Add option for debugging

--- src/middlewares/commonReply.js
+++ src/middlewares/commonReply.js
@@ -1,14 +1,23 @@
+const moduleName = 'fetchBaseQuery/middlewares/commonReply'
+
 export default () => query => async (options) => {
     const { status, data, error, headers } = await query(options)
+    let reply
     if (error) {
-        return { status, error: true, message: error.message, headers }
+        reply = { status, error: true, message: error.message || error, headers }
     } else if (data) {
         if (data.success) {
-            return { ...data, status, headers }
+            reply = { ...data, status, headers }
         } else if (data.error) {
-            return { status, error: true, message: data.message, headers }
+            reply = { status, error: true, message: data.message, headers }
+        } else {
+            reply = { status, error: true, message: 'Unexpected format of common response body.', headers }
         }
-        return { status, error: true, message: 'Unexpected format of common response body.', headers }
+    } else {
+        reply = { status, headers }
     }
-    return { status, headers }
+    if (options.debug?.commonReply) {
+        console.log(moduleName, reply)
+    }
+    return reply
 }
