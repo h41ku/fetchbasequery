d1474e85aa6560e6c6983b8a24c7e3045f2ce4ea
h41ku <h41ku.c0de@ya.ru>
2025-02-27 14:50:28 +0300

    Add option for debugging

--- src/middlewares/jsonReply.js
+++ src/middlewares/jsonReply.js
@@ -1,3 +1,5 @@
+const moduleName = 'fetchBaseQuery/middlewares/jsonReply'
+
 export default (settings = {}) => query => async (options = {}) => {
     const {
         isJsonBody
@@ -7,19 +9,27 @@ export default (settings = {}) => query => async (options = {}) => {
     }
     const response = await query(options)
     const { status, error, headers } = response
+    let reply
     if (error) {
-        return { status, error, headers }
-    }
-    let data
-    try {
-        if (isJsonBody(response) && response.json) {
-            data = await response.json()
+        reply = { status, error, headers }
+    } else {
+        let data
+        try {
+            if (isJsonBody(response)) {
+                data = await response.json()
+                reply = { status, data, headers }
+            } else {
+                reply = { status, headers }
+            }
+        } catch (error) {
+            if (options.logErrors) {
+                console.error(moduleName, error)
+            }
+            reply = { status, error, headers }
         }
-    } catch (error) {
-        if (options.logErrors) {
-            console.error('fetchBaseQuery/middlewares/jsonReply', error)
-        }
-        return { status, error, headers }
     }
-    return { status, data, headers }
+    if (options.debug?.jsonReply) {
+        console.log(moduleName, reply)
+    }
+    return reply
 }
