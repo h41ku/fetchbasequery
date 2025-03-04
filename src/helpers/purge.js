d0279fe68ba056cec95be6e0e747052c6f8c0d34
h41ku <h41ku.c0de@ya.ru>
2025-03-04 15:00:54 +0300

    Add support for instance of `Headers`

--- src/helpers/purge.js
+++ src/helpers/purge.js
@@ -1,4 +1,16 @@
-export default object => Object.fromEntries(
+const purgeObject = object => Object.fromEntries(
     Object.entries(object)
         .filter(entry => entry[1] !== undefined)
 )
+
+const purgeHeaders = headers => {
+    headers.entities().forEach(([ name, value ]) => {
+        if (value === '')
+            headers.delete(name)
+    })
+    return headers
+}
+
+export default object => object instanceof Headers
+    ? purgeHeaders(object)
+    : purgeObject(object)
