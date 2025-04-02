c3b28fa70d2dcbaffdacc821f8c814087d6408ae
h41ku <h41ku.c0de@ya.ru>
2025-04-02 14:39:01 +0300

    Return abortable promise

--- src/fetchBaseQuery.js
+++ src/fetchBaseQuery.js
@@ -1,9 +1,10 @@
 // const pipeAsync = (...fns) => (input) => fns.reduce((chain, func) => chain.then(func), Promise.resolve(input))
+import abortable from './helpers/abortable.js'
 import pipe from './helpers/pipe.js'
 import defaultBaseQuery from './baseQuery.js'
 
 export default ({ baseQuery, middlewares, ...defaultOptions } = {}) => {
-    const pipedQuery = pipe(...(middlewares || []))(baseQuery || defaultBaseQuery)
+    const pipedQuery = pipe(...(middlewares || []).map(abortable))(baseQuery || defaultBaseQuery)
     return options => pipedQuery({
         ...defaultOptions,
         ...options
